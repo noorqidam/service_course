0e94a9a7246f245251e067408600aa53a52577ba
noorqidam <noorqidam@gmail.com>
2021-03-04 17:23:42 +0700

    add model lesson and relations

---
+++ app/Models/Lesson.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lesson extends Model
+{
+    use HasFactory;
+
+    protected $table = 'lessons';
+
+    protected $fillable = [
+        'name', 'video', 'chapter_id'
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:m:s',
+        'updated_at' => 'datetime:Y-m-d H:m:s',
+    ];
+
+    public function chapter()
+    {
+        return $this->belongsTo(Chapter::class);
+    }
+}
