f2882fb47ff2b4df1e3c59a320b052a84e58cc0b
noorqidam <noorqidam@gmail.com>
2021-03-05 09:31:04 +0700

    add API update Mentor

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('mentors', [MentorController::class, 'create']);
+Route::post('mentors/{id}', [MentorController::class, 'update']);
